78ed592f0efb5fe0c4d381cc900a8fd844fe8c8e
Daniel Stroh <danielsstroh@gmail.com>
2014-04-12 20:31:41 -0400

    updates

--- Maps_HackRU_Spring_2014/src/parser/Xml2GeoJSON.java
+++ Maps_HackRU_Spring_2014/src/parser/Xml2GeoJSON.java
@@ -1,5 +1,43 @@
 package parser;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONObject;
+import org.json.XML;
+
+
 public class Xml2GeoJSON {
 
+	public Xml2GeoJSON() {
+		try {
+			String str = "http://maps.google.com/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true";
+			URL url = new URL(str);
+			
+			InputStream is = url.openStream();
+			int ptr = 0;
+			StringBuilder builder = new StringBuilder();
+			while ((ptr = is.read()) != -1) {
+			    builder.append((char) ptr);
+			}
+			String xml = builder.toString();
+			
+			//convert XML to JSONObject
+			JSONObject jsonObject = XML.toJSONObject(xml);
+			System.out.println(jsonObject);
+		} 
+		//new URL()
+		catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//URL.openStream()
+		//InputStream.read()
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }

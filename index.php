ebe1f0b71c0616bdcde81b856ce0c3430f844999
rayedchan <rayedchan@hotmail.com>
2014-04-13 07:14:39 -0400

    - All bus routes GeoJson data files

--- index.php
+++ index.php
@@ -21,8 +21,7 @@
             
             $(document).ready(function()
             {
-                var route_colors = ["#FF0000", "#00FF00", "#0000FF", "#FF3300", "#00FF33", "#3300FF", "#FF0033", "#33FF00", "#FF3333", "#33FF33", "#3333FF", "#FFCC00", "#00FFCC", "#CC00F"];
-                
+                var route_colors = ["#FF0000", "#00FF00", "#0000FF", "#FF3300", "#00FF33", "#3300FF", "#FF0033", "#33FF00", "#FF3333", "#33FF33", "#3333FF", "red", "#00FFCC", "#CC00F"];
                 
                 // Uses API from Rutgers to get bus routes; Gets String representation of XML 
                 var xmlBusRoutesRequest = httpGet("http://webservices.nextbus.com/service/publicXMLFeed?a=rutgers&command=routeConfig");
@@ -58,7 +57,13 @@
                 for(var z = 0; z < buses.length; z++)
                 {
                     // Construct an entire bus route by connecting all the given paths 
-                    var route = buses[z]['path'];                  
+                    var route = buses[z]['path']; 
+                    
+                    // Get the bus title
+                    var bus_title = buses[z]['title'];
+                    console.log(bus_title);
+                    
+                    //if(bus_title!="New Brunsquick 2 Shuttle"){continue;}
 
                     // Iterate each path in a given route
                     for(var j = 0; j < route.length; j++)
@@ -104,7 +109,7 @@
                 {
                     var mapOptions = {
                       center: new google.maps.LatLng(40.49957, -74.44824),
-                      zoom: 18
+                      zoom: 14
                     };
                     var map = new google.maps.Map(document.getElementById("map-canvas"),
                         mapOptions);
@@ -126,7 +131,21 @@
                      });
 
                     //Load a GeoJSON from the same server as our demo.
-                    map.data.loadGeoJson('json/samplePath.json');
+                    //map.data.loadGeoJson('json/allBusRoutes.json');
+                    //map.data.loadGeoJson('json/bus_A.json');
+                    //map.data.loadGeoJson('json/bus_B.json');
+                    //map.data.loadGeoJson('json/bus_C.json');
+                    //map.data.loadGeoJson('json/bus_EE.json');
+                    //map.data.loadGeoJson('json/bus_F.json');
+                    //map.data.loadGeoJson('json/bus_H.json');
+                    map.data.loadGeoJson('json/bus_LX.json');
+                    //map.data.loadGeoJson('json/bus_REXB.json');
+                    //map.data.loadGeoJson('json/bus_REXL.json');
+                    //map.data.loadGeoJson('json/bus_allcampus.json');
+                    //map.data.loadGeoJson('json/bus_NBS1.json');
+                    //map.data.loadGeoJson('json/bus_NBS2.json');
+                    //map.data.loadGeoJson('json/bus_weekend1.json');
+                    //map.data.loadGeoJson('json/bus_weekend2.json');
                 }
 
 		google.maps.event.addDomListener(window, 'load', initialize);   
